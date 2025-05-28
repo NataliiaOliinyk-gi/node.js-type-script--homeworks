8c5e90492620209f2549cb2cc2c16c4348a47c8e
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-05-28 23:14:59 +0200

    update homework_01

--- homework_01/src/logger.js
+++ homework_01/src/logger.js
@@ -1,8 +1,9 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
+const filepath = path.resolve("src", "log.txt");
+
 const logMessage = async (message) => {
-    const filepath = path.resolve("src", "log.txt");
     await fs.appendFile(filepath, `\n${message}`);
 };
 
